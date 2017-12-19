757ea16fbdba3089658eff7dbb45305cf8190813
welt101 <adrian.schubek@gmail.com>
2017-12-19 17:29:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,11 @@ var bot = new Discord.Client();
 bot.on('message',message => {
        var sender = message.author;
        var msg = message.content.toUpperCase();
-       var prefix = ">";
+       var prefix = "<";
     if (msg === prefix + 'PING') {
-        message.channel.send('Pong!')
+        message.channel.send('Pong!');
+    } else if (msg === prefix + 'PLAY) {
+        message.channel.send('Play Open Driving here: http://roblox.com/games/726302522/Open-Driving');
     }
 });
 bot.login(process.env.BOT_TOKEN);
