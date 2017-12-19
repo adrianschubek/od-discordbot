c87c8f257e17d0b9be9d76571820f609876ffcbb
welt101 <adrian.schubek@gmail.com>
2017-12-19 18:27:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,14 +19,19 @@ bot.on('message',message => {
     } else if (msg === prefix + 'WEBSITE') {
          message.channel.send(sender + ' Our website -> http://www.opendriving.tk');    
     } else if (msg === prefix + 'HELP') {
-         message.channel.send('I\'m a bot developed by @welt101#5653. Available cmds:\n**^^play** - Link to our roblox game\n**^^whoami** - Who am I?\n**^^apply** - You this cmd if you want to apply.\n**^^report** - Report a player.\n**^^website** - Open our website.');    
+         message.channel.send('I\'m a bot developed by @welt101#5653. Available cmds:\n**^^play** - Link to our roblox game\n**^^whoami** - Who am I?\n**^^apply** - Use this cmd if you want to apply.\n**^^report** - Report a player.\n**^^website** - Open our website.');    
     }
 });
 
 bot.on('guildMemberAdd', member => {
-       console.log(member + ' >> joined');
+    console.log(member + ' >> joined');
     var role = member.guild.roles.find('name', 'Fans');
     member.addRole(role);
+});
+
+bot.on('ready', () => {
+    bot.user.setStatus('Online');
+    bot.user.setGame('say ^^help');
 });
 
 bot.login(process.env.BOT_TOKEN);
