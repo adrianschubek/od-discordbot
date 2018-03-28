5698a6791ffe2c42f1fd2e1bcad08ecdf96185d7
welt101 <adrian.schubek@gmail.com>
2018-03-28 22:05:48 +0200

    s

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ bot.on('message',message => {
             message.channel.send(sender + ' Our website -> http://www.opendriving.tk');    
     } else if (msg === prefix + 'HELP') {
      const e = new Discord.RichEmbed()
-     .setColor("#009688").setTitle("Help for OD Bot")
+     .setColor('AQUA').setTitle("Help for OD Bot")
      .setDescription('*I\'m a bot developed by **welt101#5653***.\n [ Available commands ]\n**+play**/**+games** - Shows a list of our games\n**+wiki** - Opens our wiki\n**+forum**/**+support** - Opens our support site\n**+whoami** - Who am I?\n**+apply** - Use this cmd if you want to apply.\n**+report** - Report a player.\n**+website** - Open our website.\n**+afk** - Change your status to *afk*.\n**+unafk** - Change your status back to normal.\n**+userinfo** - Information about you.\n [ In-Game commands **WIP** ]\n**+money [USERNAME]** - Returns the username\'s money');
         message.channel.send({e});    
     } else if ((msg === prefix + 'LIST')||(msg === prefix + 'GAMES')||(msg === prefix + 'PLAY')) {
