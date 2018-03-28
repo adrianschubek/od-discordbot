a83787fb793098b1e5f10e3edc6890fd4a623c16
welt101 <adrian.schubek@gmail.com>
2018-03-28 23:36:41 +0200

    g

--- bot.js
+++ bot.js
@@ -57,7 +57,7 @@ bot.on('message',message => {
         const embed = new Discord.RichEmbed()
        .setColor(0x00AE86)
        .setTitle("Help for OD Bot")
-       .setDescription('I\'m a bot developed by **welt101#5653**.\n\n [ Available commands ]\n\n**+play**/**+games** - Shows a list of our games\n**+wiki** - Opens our wiki\n**+forum**/**+support** - Opens our support site\n**+whoami** - Who am I?\n**+apply** - Use this cmd if you want to apply.\n**+report** - Report a player.\n**+website** - Open our website.\n**+afk** - Change your status to *afk*.\n**+unafk** - Change your status back to normal.\n**+userinfo** - Information about you.\n\n [ In-Game commands **WIP** ]\n\n**+money [USERNAME]** - Returns how much money the user has in-game ');
+       .setDescription('I\'m a bot developed by **welt101#5653**.\n\n [ Available commands ]\n\n**+play**/**+games** - Shows a list of our games\n**+wiki** - Opens our wiki\n**+forum**/**+support** - Opens our support site\n**+whoami** - Who am I?\n**+apply** - Use this cmd if you want to apply.\n**+report** - Report a player.\n**+website** - Open our website.\n**+afk** - Change your status to *afk*.\n**+unafk** - Change your status back to normal.\n**+userinfo** - Information about you.\n\n [ IN-GAME commands across all games (**WIP**) ]\n\n**+money [USERNAME]** - Returns the money the user has.\n**+miles [USERNAME]** - Returns the amount of miles the user has driven.');
       
        message.channel.send({embed});   
     } else if (msg.startsWith(prefix + 'MONEY')) {   
@@ -68,7 +68,14 @@ bot.on('message',message => {
         const embed = new Discord.RichEmbed()            
         .setColor(3447003)
         .setTitle("")
-        .setDescription('**' + text + '\'s** Cash in Open Driving:\n  ' + _b);
+        .setDescription('**' + text + '\'s** Cash in Open Driving:\n  ' + _b + ' $');
+        message.channel.send({embed})
+    } else if (msg.startsWith(prefix + 'MILES')) {
+        message.delete(); 
+        const embed = new Discord.RichEmbed()            
+        .setColor(0xFF0000)
+        .setTitle("*Coming Soon :tm:*")
+        .setDescription("Really soon..");
         message.channel.send({embed})
     } else if ((msg === prefix + 'LIST')||(msg === prefix + 'GAMES')||(msg === prefix + 'PLAY')) {
         message.channel.send('A complete list of our games can be found here: http://opendriving.wikia.com/Games');
