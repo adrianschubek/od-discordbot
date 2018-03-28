987a3c92004dd7383afef91df8121a5019440a18
welt101 <adrian.schubek@gmail.com>
2018-03-28 21:23:10 +0200

    dd

--- bot.js
+++ bot.js
@@ -22,8 +22,6 @@ bot.on('message',message => {
      
     if (msg === prefix + 'PING') {
         message.channel.send('Pong!');
-    } else if (msg === prefix + 'PLAY') {
-        message.channel.send('Play Open Driving here: http://roblox.com/games/726302522/Open-Driving');
     } else if (msg === prefix + 'APPLY') {			
         message.channel.send(sender+' Our roblox application centre is currently closed, if you want to apply then post your full application in #temp-applications');
     } else if (msg === prefix + 'WHOAMI') {
@@ -34,7 +32,7 @@ bot.on('message',message => {
          message.channel.send(sender + ' Our website -> http://www.opendriving.tk');    
     } else if (msg === prefix + 'HELP') {
          message.channel.send('I\'m a bot developed by **welt101#5653**. Available cmds:\n**+play** - Link to our main roblox game\n**+games** - List of our games\n**+whoami** - Who am I?\n**+apply** - Use this cmd if you want to apply.\n**+report** - Report a player.\n**+website** - Open our website.\n**+afk** - Change your status to *afk*.\n**+unafk** - Change your status back to normal.\n**+userinfo** - Information about you.');    
-    } else if ((msg === prefix + 'LIST')||(msg === prefix + 'GAMES')) {
+    } else if ((msg === prefix + 'LIST')||(msg === prefix + 'GAMES')||(msg === prefix + 'PLAY')) {
         message.channel.send('**--- LIST OF OUR GAMES ---**\n**Open Driving \(Main game\)**: http://bit.ly/opendriving \n**Open Driving: Palms Bay**: http://bit.ly/od-palmsbay');
     } else if (msg === prefix) {
         message.channel.send('*Invalid command! Use **+help** for help.*');
